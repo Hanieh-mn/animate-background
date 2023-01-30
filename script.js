e260c12ef522dc2044b9c5dc166ab81a4a1d5401
Hannn18 <ha.m99019@gmail.com>
2023-01-30 10:23:07 +0330

    adding files

---
+++ script.js
@@ -0,0 +1,12 @@
+const body = document.querySelector('body');
+
+function add() {
+    body.style.backgroundColor = 'rgb(38, 37, 37)';
+    body.style.transition = '.4s all ease';
+}
+
+
+function remove() {
+    body.style.backgroundColor = '#d5d2cd';
+    body.style.transition = '.4s all ease';
+}
